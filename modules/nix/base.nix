c4d5f9258c717cd97945e4108894d512ea802bc9
voidptrr <bruno.tommaso@protonmail.com>
2026-03-05 23:15:12 +0000

    chore: improve nix base modules

--- modules/nix/base.nix
+++ modules/nix/base.nix
@@ -1,19 +1,22 @@
 {lib, ...}: {
   flake.nixModules.base = {pkgs, ...}: {
-    nix = {
-      enable = pkgs.stdenv.hostPlatform.isLinux;
-      settings = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
-        auto-optimise-store = true;
-        warn-dirty = false;
-      };
-      gc = lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
-        automatic = true;
-        interval = {
-          Hour = 3;
-          Minute = 15;
+    nix =
+      {
+        enable = pkgs.stdenv.hostPlatform.isLinux;
+      }
+      // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+        settings = {
+          auto-optimise-store = true;
+          warn-dirty = false;
         };
-        options = "--delete-older-than 5d";
+        gc = {
+          automatic = true;
+          interval = {
+            Hour = 3;
+            Minute = 15;
+          };
+          options = "--delete-older-than 5d";
+        };
       };
-    };
   };
 }
